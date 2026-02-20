03a419ffcdb510d637d6d93c69c7e65f76565f41
Klaus Creator <abushnatanem@gmail.com>
2026-02-20 16:37:24 +0300

    feat: add the important configuration

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,8 +2,39 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import { ConfigModule } from '@nestjs/config';
+import { PrismaModule } from './modules/prisma/prisma.module';
+
+// THIRD PARTY MODULES
+import { AuthModule } from '@thallesp/nestjs-better-auth';
+
+import configuration from './config/env.config';
+import { envValidationSchema } from './common/validation/env.validation';
+import { auth } from './lib/auth';
+
 @Module({
-  imports: [],
+  imports: [
+    // Configuration Modules
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+      load: [configuration],
+      ignoreEnvFile: process.env.NODE_ENV === 'production',
+      validationSchema: envValidationSchema,
+      validationOptions: {
+        abortEarly: true,
+        allowUnknown: true,
+      },
+      expandVariables: true,
+    }),
+
+    // Third Party Modules
+    AuthModule.forRoot({ auth }),
+
+    // Local Modules
+    PrismaModule,
+  ],
+
   controllers: [AppController],
   providers: [AppService],
 })
