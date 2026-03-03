43299e83487fa32d7a1843f332df725a6021b51e
The Original <abushnatanem@gmail.com>
2026-03-02 22:45:33 -0500

    update validation schema for login

--- client/lib/validations/login.validation.ts
+++ client/lib/validations/login.validation.ts
@@ -3,5 +3,5 @@ export const formSchema = z.object({
   email: z.string().email({ message: "Please enter a valid email address" }),
   password: z
     .string()
-    .min(8, { message: "Password must be at least 8 characters" }),
+    .min(6, { message: "Password must be atleast 6" }),
 });
