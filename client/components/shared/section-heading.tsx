90826022f55939e214dfc64afcfc8270464639ec
The Original <abushnatanem@gmail.com>
2026-03-06 11:32:37 -0500

    logo change

--- client/components/shared/section-heading.tsx
+++ client/components/shared/section-heading.tsx
@@ -7,12 +7,12 @@ interface ISectionHeading {
 
 export default function SectionHeading ({title1, title2, desc, center} : ISectionHeading) {
     return <div className={`w-full flex  flex-col gap-2  ${center ? "text-center items-center" : "items-start"}`}>
-        <h2 className="text-2xl lg:text-3xl 2xl:text-4xl font-clash tracking-[1px]">
+        <h2 className="text-xl lg:text-2xl 2xl:text-3xl font-clash tracking-[1px]">
             { title1 }
             { title2 && <span className="text-primary"> { title2 } </span> }
         </h2>
 
-        <p className="text-foreground-secondary tracking-[1px] font-satoshi text-base lg:text-xl">
+        <p className="text-foreground-tertiary tracking-[1px] font-satoshi text-base lg:text-xl">
             { desc }
         </p>
     </div>
