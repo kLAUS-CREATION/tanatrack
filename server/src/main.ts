361a30cd073acc825a0ca00fc73b80c16bf1ac9b
The Original <abushnatanem@gmail.com>
2026-03-06 11:38:11 -0500

    cors update

--- server/src/main.ts
+++ server/src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   });
 
   app.enableCors({
-    origin: ['http://localhost:3000', 'http://localhost:5500'],
+    origin: ['http://localhost:3000', 'http://localhost:3001', 'http://localhost:5500'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
     allowedHeaders: ['Content-Type', 'Authorization', 'Origin'],
